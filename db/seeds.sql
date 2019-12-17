bbaf689dd4509c79dffeb8480a34be694c3864bc
Austin Williams <auswil13@gmail.com>
2019-12-16 22:37:07 -0600

    firsfirst push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE DATABASE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheese Burger', false), ('Alpine Burger', false), ('Bacon Cheese Burger', false);
+
+SELECT * FROM burgers;
